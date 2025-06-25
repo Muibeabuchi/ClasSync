80e6f22e30c734afd7c02f7dc0e21099d6e71f9f
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-06-25 17:57:31 +0100

    CHORE:Wrap the application in a Convex betterAuth provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,33 @@
 /// <reference types="vite/client" />
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+import * as React from "react";
+import { Toaster } from "react-hot-toast";
+import { ConvexReactClient } from "convex/react";
+
 import {
-  Link,
   Outlet,
   createRootRouteWithContext,
   useRouterState,
   HeadContent,
   Scripts,
+  useRouteContext,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import * as React from "react";
-import { Toaster } from "react-hot-toast";
-import type { QueryClient } from "@tanstack/react-query";
-import { DefaultCatchBoundary } from "@/components/DefaultCatchBoundary";
-import { NotFound } from "@/components/NotFound";
 import appCss from "@/styles/app.css?url";
 import { seo } from "@/utils/seo";
 import { Loader } from "@/components/Loader";
+import { NotFound } from "@/components/NotFound";
+import { fetchAuth } from "@/lib/utils";
+import { authClient } from "@/lib/auth-client";
+import type { QueryClient } from "@tanstack/react-query";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+import { DefaultCatchBoundary } from "@/components/DefaultCatchBoundary";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
+  convexClient: ConvexReactClient;
+  convexQueryClient: ConvexQueryClient;
 }>()({
   head: () => ({
     meta: [
@@ -66,15 +74,35 @@ export const Route = createRootRouteWithContext<{
       </RootDocument>
     );
   },
+  beforeLoad: async (ctx) => {
+    // all queries, mutations and action made with TanStack Query will be
+    // authenticated by an identity token.
+    const auth = await fetchAuth();
+    const { userId, token } = auth;
+
+    // During SSR only (the only time serverHttpClient exists),
+    // set the auth token for Convex to make HTTP queries with.
+    if (token) {
+      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
+    }
+
+    return { userId, token };
+  },
   notFoundComponent: () => <NotFound />,
   component: RootComponent,
 });
 
 function RootComponent() {
+  const context = useRouteContext({ from: Route.id });
   return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
+    <ConvexBetterAuthProvider
+      client={context.convexClient}
+      authClient={authClient}
+    >
+      <RootDocument>
+        <Outlet />
+      </RootDocument>
+    </ConvexBetterAuthProvider>
   );
 }
 
