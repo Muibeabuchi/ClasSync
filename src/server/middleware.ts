d39f47160a7f78de85a6d676387418ae1f99d828
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-06-30 03:22:23 +0100

    FEAT: create auth middleware for server functions REFACTOR: remove the auth guard in dashboard route's beforeLoad function

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -2,6 +2,7 @@ import { getCookieName } from '@convex-dev/better-auth/react-start';
 import { createAuth } from '../../convex/auth';
 import { ConvexHttpClient } from 'convex/browser';
 import { getCookie } from '@tanstack/react-start/server';
+import { createMiddleware } from '@tanstack/react-start';
 
 const setupClient = (token?: string) => {
   const client = new ConvexHttpClient(import.meta.env.VITE_CONVEX_URL);
@@ -16,4 +17,13 @@ const getToken = async () => {
   return getCookie(sessionCookieName);
 };
 
-export { getToken, setupClient };
+export const authMiddleware = createMiddleware({ type: 'function' }).server(
+  async ({ next }) => {
+    const token = await getToken();
+    return next({
+      context: {
+        convex: setupClient(token),
+      },
+    });
+  },
+);
--- src/server/userprofile.ts
+++ src/server/userprofile.ts
@@ -1,27 +1,27 @@
 import { createServerFn } from '@tanstack/react-start';
-import { getToken, setupClient } from './middleware';
+import { authMiddleware } from './middleware';
 import { api } from 'convex/_generated/api';
 
 export const getUserOnboardStatusAction = createServerFn({
   method: 'GET',
-}).handler(async () => {
-  const token = await getToken();
-  try {
-    return await setupClient(token).query(
-      api.userProfile.getUserOnboardedStatus,
-    );
-  } catch {
-    return null;
-  }
-});
+})
+  .middleware([authMiddleware])
+  .handler(async ({ context }) => {
+    try {
+      return await context.convex.query(api.userProfile.getUserOnboardedStatus);
+    } catch {
+      return null;
+    }
+  });
 
 export const getUserRoleAction = createServerFn({
   method: 'GET',
-}).handler(async () => {
-  const token = await getToken();
-  try {
-    return await setupClient(token).query(api.userProfile.getUserRole);
-  } catch {
-    console.log('An error occurred when fetching the user role info ');
-  }
-});
+})
+  .middleware([authMiddleware])
+  .handler(async ({ context }) => {
+    try {
+      return await context.convex.query(api.userProfile.getUserRole);
+    } catch {
+      console.log('An error occurred when fetching the user role info ');
+    }
+  });
